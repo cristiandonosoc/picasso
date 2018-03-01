0012429d18d1489e9512898d37f2887d22c2a86c
cristiandonosoc <cristiandonosoc@gmail.com>
2018-03-01 01:13:57 -0800

    Support SDL2 loading correctly

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 #include <SDL.h>
 
 int main(int, char **) {
-  if (!SDL_Init(SDL_INIT_VIDEO)) {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
     return 1;
   }
