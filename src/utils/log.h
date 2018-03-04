37b609a10396ade80e398c66d55ffb35e9b2f89e
cristiandonosoc <cristiandonosoc@gmail.com>
2018-03-03 22:42:39 -0800

    Correctly defined variables not in header
    It only worked because only one translation unit was using
    the loggers.

--- src/utils/log.h
+++ src/utils/log.h
@@ -7,51 +7,31 @@
 namespace picasso {
 namespace utils {
 
-#define DEFINE_LOG_FUNC(func_name, prefix, file)                  \
-  void func_name(const char *fmt, ...) {                   \
-    char buf[1024];                                                       \
-    snprintf(buf, sizeof(buf), prefix " %s\n", fmt);   \
-    va_list arglist;                                                      \
-    va_start(arglist, fmt);                                               \
-    vfprintf(file, buf, arglist);                                         \
-    fflush(file);                                                         \
-    va_end(arglist);                                                      \
-  }
-
-
-
-#define DEFINE_INDENT_LOG_FUNC(func_name, prefix, file)                  \
-  void func_name(int indent, const char *fmt, ...) {                   \
-    char buf[1024];                                                       \
-    snprintf(buf, sizeof(buf), prefix " %*s%s\n", indent, " ", fmt);   \
-    va_list arglist;                                                      \
-    va_start(arglist, fmt);                                               \
-    vfprintf(file, buf, arglist);                                         \
-    fflush(file);                                                         \
-    va_end(arglist);                                                      \
-  }
-
 namespace logout {
 
-DEFINE_LOG_FUNC(Info, "[INFO]", stdout);
-DEFINE_LOG_FUNC(Warn, "[WARN]", stdout);
-DEFINE_LOG_FUNC(Error, "[ERROR]", stdout);
+void Info(const char *fmt, ...);
+void Warn(const char *fmt, ...);
+void Error(const char *fmt, ...);
+void Debug(const char *fmt, ...);
 
-DEFINE_INDENT_LOG_FUNC(IndentInfo, "[INFO]", stdout);
-DEFINE_INDENT_LOG_FUNC(IndentWarn, "[WARN]", stdout);
-DEFINE_INDENT_LOG_FUNC(IndentError, "[ERROR]", stdout);
+void IndentInfo(size_t indent, const char *fmt, ...);
+void IndentWarn(size_t indent, const char *fmt, ...);
+void IndentError(size_t indent, const char *fmt, ...);
+void IndentDebug(size_t indent, const char *fmt, ...);
 
 }   // namespace logout
 
 namespace logerr {
 
-DEFINE_LOG_FUNC(Info, "[INFO]", stderr);
-DEFINE_LOG_FUNC(Warn, "[WARN]", stderr);
-DEFINE_LOG_FUNC(Error, "[ERROR]", stderr);
+void Info(const char *fmt, ...);
+void Warn(const char *fmt, ...);
+void Error(const char *fmt, ...);
+void Debug(const char *fmt, ...);
 
-DEFINE_INDENT_LOG_FUNC(IndentInfo, "[INFO]", stderr);
-DEFINE_INDENT_LOG_FUNC(IndentWarn, "[WARN]", stderr);
-DEFINE_INDENT_LOG_FUNC(IndentError, "[ERROR]", stderr);
+void IndentInfo(size_t indent, const char *fmt, ...);
+void IndentWarn(size_t indent, const char *fmt, ...);
+void IndentError(size_t indent, const char *fmt, ...);
+void IndentDebug(size_t indent, const char *fmt, ...);
 
 }   // namespace logerr
 
